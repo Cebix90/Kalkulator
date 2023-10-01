90d34f573eb1f15930a0b3efaa60a65b3c3cc010
Cebix90 <cebulmat@gmail.com>
2023-10-01 16:46:00 +0200

    Initial commit

---
+++ Kalkulator/Models/Calculator.cs
@@ -0,0 +1,11 @@
+﻿namespace Kalkulator.Models
+{
+    public class Calculator
+    {
+        public int FirstNumber { get; set; }
+        public int SecondNumber { get; set; }
+        public char Mark { get; set; }
+        
+        public int Result { get; set; }
+    }
+}
